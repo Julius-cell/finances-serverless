b2edc3a66e3b7dab3eebb43e8931e63082774f4e
juliocid <julio.cid@femsasalud.com>
2025-02-13 16:21:40 +0100

    feat: update tests

--- src/app/auth/auth.spec.ts
+++ src/app/auth/auth.spec.ts
@@ -4,31 +4,34 @@ import { AuthService } from "./auth.service";
 import { of } from "rxjs";
 import { Router } from "@angular/router";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { FormsModule, NgForm } from "@angular/forms";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
-jest.mock("./auth.service");
+const UserState = {
+  data: {} as any,
+  error: null,
+  success: true,
+};
+
+const formValue = {
+  value: { email: "test@mail.com", password: "1234" },
+};
 
 describe("AuthComponent", () => {
   let component: AuthComponent;
-  let authService: jest.Mocked<AuthService>;
   let router: jest.Mocked<Router>;
+  let authService: jest.Mocked<AuthService>;
 
   beforeEach(() => {
-    authService = {
-      userState: {
-        value: { success: false, data: null, error: null },
-        set: jest.fn(),
-      },
-      createUser: jest.fn(),
-      loginUser: jest.fn(),
-      logOutUser: jest.fn(),
-      resetPassword: jest.fn(),
-      getErrorMessageRegister: jest.fn(),
-      getErrorMessageLogin: jest.fn(),
-    } as unknown as jest.Mocked<AuthService>;
-
     router = {
       navigate: jest.fn(),
     } as unknown as jest.Mocked<Router>;
+
+    authService = {
+      createUser: jest.fn(),
+      loginUser: jest.fn(),
+      resetPassword: jest.fn(),
+    } as unknown as jest.Mocked<AuthService>;
 
     const fixture = TestBed.overrideComponent(AuthComponent, {
       set: {
@@ -36,101 +39,121 @@ describe("AuthComponent", () => {
           { provide: AuthService, useValue: authService },
           { provide: Router, useValue: router },
         ],
-        imports: [],
+        imports: [FormsModule, FontAwesomeModule],
         schemas: [NO_ERRORS_SCHEMA],
       },
     }).createComponent(AuthComponent);
 
     component = fixture.componentInstance;
+    // Spy component methods
+    jest.spyOn(component, "createUser");
+    jest.spyOn(component, "loginUser");
+    jest.spyOn(component.error, "set");
   });
 
-  describe("createUser", () => {
+  describe("handleSubmit", () => {
     it("should set error message on invalid form", () => {
-      component.createUser({
+      component.handleSubmit({
         invalid: true,
-      } as any);
+      } as NgForm);
 
-      expect(authService.createUser).not.toHaveBeenCalled();
-      expect(router.navigate).not.toHaveBeenCalled();
+      expect(component.createUser).not.toHaveBeenCalled();
+      expect(component.loginUser).not.toHaveBeenCalled();
       expect(component.error()).toBe("Por favor, completa todos los campos.");
     });
 
-    it("should navigate to dashboard on successful register", () => {
-      authService.createUser = jest.fn().mockReturnValue(of({ success: true }));
+    it("should execute createUser", () => {
+      authService.createUser.mockReturnValue(of(UserState));
 
-      component.createUser({
-        value: { email: "test@mail.com", password: "1234" },
-      } as any);
+      component.isLogin.set(false);
+
+      component.handleSubmit(formValue as NgForm);
+
+      expect(component.createUser).toHaveBeenCalledWith(formValue);
+      expect(component.createUser).toHaveBeenCalledTimes(1);
+      expect(component.loginUser).not.toHaveBeenCalled();
+      expect(component.error()).toBe("");
+    });
+
+    it("should execute loginUser", () => {
+      authService.loginUser.mockReturnValue(of(UserState));
+
+      component.isLogin.set(true);
+
+      component.handleSubmit(formValue as NgForm);
+
+      expect(component.loginUser).toHaveBeenCalledWith(formValue);
+      expect(component.loginUser).toHaveBeenCalledTimes(1);
+      expect(component.createUser).not.toHaveBeenCalled();
+      expect(component.error()).toBe("");
+    });
+  });
+
+  describe("createUser", () => {
+    it("should navigate to dashboard on successful register", () => {
+      authService.createUser.mockReturnValue(of({ success: true }));
+
+      component.createUser(formValue as NgForm);
 
       expect(authService.createUser).toHaveBeenCalledWith(
-        "test@mail.com",
-        "1234"
+        formValue.value.email,
+        formValue.value.password
       );
       expect(router.navigate).toHaveBeenCalledWith(["/dashboard"]);
     });
 
     it("should set error message on failed login (API error)", () => {
-      authService.createUser = jest.fn().mockReturnValue(
+      authService.createUser.mockReturnValue(
         of({
           success: false,
           error: "Ha habido un problema. Por favor intenta otra vez.",
         })
       );
 
-      component.createUser({
-        value: { email: "test@mail.com", password: "1234" },
-      } as any);
+      component.createUser(formValue as NgForm);
 
       expect(authService.createUser).toHaveBeenCalledWith(
-        "test@mail.com",
-        "1234"
+        formValue.value.email,
+        formValue.value.password
       );
       expect(router.navigate).not.toHaveBeenCalled();
+      expect(component.error.set).toHaveBeenCalledWith(
+        "Ha habido un problema. Por favor intenta otra vez."
+      );
     });
   });
 
   describe("loginUser", () => {
-    it("should set error message on invalid form", () => {
-      component.loginUser({
-        invalid: true,
-      } as any);
-
-      expect(authService.loginUser).not.toHaveBeenCalled();
-      expect(router.navigate).not.toHaveBeenCalled();
-      expect(component.error()).toBe("Por favor, completa todos los campos.");
-    });
-
     it("should navigate to dashboard on successful login", () => {
-      authService.loginUser = jest.fn().mockReturnValue(of({ success: true }));
+      authService.loginUser.mockReturnValue(of({ success: true }));
 
-      component.loginUser({
-        value: { email: "test@mail.com", password: "1234" },
-      } as any);
+      component.loginUser(formValue as NgForm);
 
       expect(authService.loginUser).toHaveBeenCalledWith(
-        "test@mail.com",
-        "1234"
+        formValue.value.email,
+        formValue.value.password
       );
       expect(router.navigate).toHaveBeenCalledWith(["/dashboard"]);
     });
 
     it("should set error message on failed login (API error)", () => {
-      authService.loginUser = jest.fn().mockReturnValue(
+      authService.loginUser.mockReturnValue(
         of({
           success: false,
-          error: "Ha habido un problema. Por favor intenta otra vez",
+          error: "Ha habido un problema. Por favor intenta otra vez.",
         })
       );
 
-      component.loginUser({
-        value: { email: "test@mail.com", password: "1234" },
-      } as any);
+      component.loginUser(formValue as NgForm);
 
       expect(authService.loginUser).toHaveBeenCalledWith(
-        "test@mail.com",
-        "1234"
+        formValue.value.email,
+        formValue.value.password
       );
       expect(router.navigate).not.toHaveBeenCalled();
+      expect(component.error.set).toHaveBeenCalledWith(
+        "Ha habido un problema. Por favor intenta otra vez."
+      );
     });
   });
 
@@ -145,36 +168,29 @@ describe("AuthComponent", () => {
     });
 
     it("should call resetPassword service", () => {
-      authService.resetPassword = jest.fn().mockReturnValue(of());
+      authService.resetPassword.mockReturnValue(of());
 
       component.recoverPassword({
-        value: { email: "test@mail.com" },
+        value: { email: formValue.value.email },
       } as any);
 
-      expect(authService.resetPassword).toHaveBeenCalledWith("test@mail.com");
+      expect(authService.resetPassword).toHaveBeenCalledWith(
+        formValue.value.email
+      );
       expect(component.isRecoveryPass()).toBe(false);
     });
   });
 
-  describe("navigateTo", () => {
-    beforeEach(() => {
-      expect(component.error()).toBe("");
-    });
+  describe("toggleAuthMode", () => {
+    it("should toggle login value", () => {
+      component.isLogin.set(true);
+      expect(component.isLogin()).toBe(true);
 
-    it("should switch to login view", () => {
-      component.navigateTo("login");
-      expect(component.isSignUp()).toBe(false);
-      expect(component.isRecoveryPass()).toBe(false);
-    });
+      component.toggleAuthMode();
+      expect(component.isLogin()).toBe(false);
 
-    it("should switch to sign-up view", () => {
-      component.navigateTo("sign-up");
-      expect(component.isSignUp()).toBe(true);
-    });
-
-    it("should switch to forgot-password view", () => {
-      component.navigateTo("forgot-password");
-      expect(component.isRecoveryPass()).toBe(true);
+      component.toggleAuthMode();
+      expect(component.isLogin()).toBe(true);
     });
   });
 });
