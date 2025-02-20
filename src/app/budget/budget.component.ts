5f168dbb2540943840186d810cc94c40735738cf
juliocid <julio.cid@femsasalud.com>
2025-02-20 07:19:48 -0300

    feat: add pie chart to budget component

--- src/app/budget/budget.component.ts
+++ src/app/budget/budget.component.ts
@@ -3,14 +3,33 @@ import { Component } from "@angular/core";
 import { faChartPie } from "@fortawesome/free-solid-svg-icons";
 
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
+import { BaseChartDirective } from "ng2-charts";
+import { ChartData } from "chart.js";
 
 @Component({
   selector: "or-budget",
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, BaseChartDirective],
   templateUrl: "./budget.component.html",
   styles: [``],
 })
 export class BudgetComponent {
-  faChartPie = faChartPie
+  faChartPie = faChartPie;
+
+  pieChartData: ChartData<"pie", number[], string> = {
+    labels: ["Rent", "Food", "Entertainment", "Savings"],
+    datasets: [
+      {
+        data: [500, 300, 200, 100],
+      },
+    ],
+  };
+
+  pieChartOptions = {
+    responsive: true,
+    maintainAspectRatio: false,
+    plugins: {
+      tooltip: { enabled: true }, // Equivalent to <Tooltip />
+    },
+  };
 }
