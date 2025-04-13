e93347e82f8356e384ced2216067d5ff250489b4
juliocid <julio.cid@femsasalud.com>
2025-04-13 13:19:30 -0400

    feat: add transaction form component to modal

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,13 +1,14 @@
-import { Component, inject } from "@angular/core";
+import { Component, output, inject } from "@angular/core";
 import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
 import { ModalService } from "./modal.service";
 import { AsyncPipe } from '@angular/common';
 import { ModalType } from './modal.types';
+import { TransactionFormComponent } from '../transaction-form/transaction-form.component';
 
 @Component({
   selector: "app-modal",
-  imports: [FontAwesomeModule, AsyncPipe],
+  imports: [FontAwesomeModule, AsyncPipe, TransactionFormComponent],
   templateUrl: "./modal.component.html",
   styles: `
     .modal-overlay {
@@ -44,9 +45,17 @@ export class ModalComponent {
   modalData$ = this.modalService.modalData$;
   ModalType = ModalType;
 
+  close = output();
+
   faClose = faClose;
 
   onClose(): void {
     this.modalService.close();
+    this.close.emit();
+  }
+
+  onTransactionSubmit(data: any): void {
+    console.log('Transaction submitted:', data);
+    this.onClose();
   }
 }
---
+++ src/app/shared/transaction-form/transaction-form.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject, output } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+export type TransactionType = 'Expense' | 'Income';
+
+@Component({
+  selector: 'app-transaction-form',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './transaction-form.component.html',
+})
+export class TransactionFormComponent {
+  private fb = inject(FormBuilder);
+
+  form: FormGroup = this.fb.group({
+    type: ['Expense', Validators.required],
+    amount: [null, [Validators.required, Validators.min(0)]],
+    category: ['', Validators.required],
+  });
+
+  onSubmit = output<{
+    type: TransactionType;
+    amount: number;
+    category: string;
+  }>();
+
+  onCancel = output<void>();
+
+  handleSubmit(): void {
+    if (this.form.valid) {
+      this.onSubmit.emit(this.form.value);
+    }
+  }
+} 
