b4aa545cd78d8cf0897b4834434ef5576df147e1
juliocid <julio.cid@femsasalud.com>
2025-05-05 14:06:43 -0400

    feat: add category select to transaction form

--- src/app/shared/modal/transaction-form/transaction-form.component.ts
+++ src/app/shared/modal/transaction-form/transaction-form.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, output } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { TransactionService } from '../../../services/transaction.service';
+import { CategorySelectComponent } from './category-select/category-select.component';
+import { Category } from '../../../services/category.service';
 
 export type TransactionType = 'Expense' | 'Income';
 export interface Transaction {
@@ -14,9 +16,9 @@ export interface Transaction {
 }
 
 @Component({
-  selector: 'modal-transaction-form',
+  selector: 'transaction-form',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, CategorySelectComponent],
   templateUrl: './transaction-form.component.html',
 })
 export class TransactionFormComponent {
@@ -25,7 +27,7 @@ export class TransactionFormComponent {
 
   form: FormGroup = this.fb.group({
     type: this.fb.control<TransactionType>('Expense', Validators.required),
-    name: this.fb.control<string>('', Validators.required),
+    name: this.fb.control<Category | undefined>(undefined, Validators.required),
     category: this.fb.control<string>('', Validators.required),
     amount: this.fb.control<number>(0, [Validators.required, Validators.min(0)]),
   });
@@ -38,5 +40,9 @@ export class TransactionFormComponent {
       await this.transactionService.saveTransaction(this.form.value);
       this.onSubmit.emit(this.form.value);
     }
+  }
+
+  handleSelectedCategory(category: Category): void {
+    this.form.patchValue({ category });
   }
 } 
