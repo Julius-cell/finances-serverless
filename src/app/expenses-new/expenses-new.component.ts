8c197b8362c70c05959d472e8c59d3a464068faf
juliocid <julio.cid@femsasalud.com>
2025-05-12 06:29:23 -0400

    feat: add new expenses component template

---
+++ src/app/expenses-new/expenses-new.component.ts
@@ -0,0 +1,40 @@
+import { Component, inject } from "@angular/core";
+import { ModalService } from "../shared/modal/modal.service";
+
+@Component({
+  selector: "or-expenses-new",
+  imports: [],
+  templateUrl: "./expenses-new.component.html",
+  styles: ``,
+})
+export class ExpensesNewComponent {
+  private modalService = inject(ModalService);
+
+  expenses = [
+    {
+      id: 1,
+      name: "Expense 1",
+      amount: 100,
+      status: "Pending",
+      category: "Food",
+    },
+    {
+      id: 2,
+      name: "Expense 2",
+      amount: 200,
+      status: "Pending",
+      category: "Transport",
+    },
+    {
+      id: 3,
+      name: "Expense 3",
+      amount: 300,
+      status: "Pending",
+      category: "Entertainment",
+    },
+  ];
+
+  addExpense() {
+    this.modalService.openNewTransaction();
+  }
+}
